77fe41786e5d10f916385f3885d0ebccfa290216
Byron Broughten <byron.broughten@gmail.com>
2025-10-12 10:55:25 -0500

    Adjusts triggerOnEdit

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import { TopOperator } from "./TopOperator.js";
-import { asU } from "./utilitiesAppsScript.js";
 
 function triggerFirstOfMonth() {
   const top = TopOperator.init();
   top.monthlyRentUpdate();
 }
 
-function triggerOnEdit(e: GoogleAppsScript.Events.SheetsOnEdit) {
+function triggerOnEditCustom(e: GoogleAppsScript.Events.SheetsOnEdit) {
   if (e.value === "TRUE") {
     const sheetId = e.range.getSheet().getSheetId();
     const colId = e.range.getColumn();
@@ -22,9 +21,9 @@ function resetTriggers(doResetTriggers: boolean = true) {
   if (doResetTriggers) {
     const top = TopOperator.init();
     top.test();
-    asU.trigger.deleteAll();
-    asU.trigger.addFirstOfMonth("triggerFirstOfMonth");
-    asU.trigger.addOnEdit("triggerOnEdit");
+    // asU.trigger.deleteAll();
+    // asU.trigger.addFirstOfMonth("triggerFirstOfMonth");
+    // asU.trigger.addOnEdit("triggerOnEditCustom");
   }
 }
 
