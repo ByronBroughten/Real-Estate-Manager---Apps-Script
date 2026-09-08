8af28fb0b072bd906c2c24abe5f2039d2fcaeaeb
Byron Broughten <byron.broughten@gmail.com>
2026-09-07 22:23:53 -0500

    Adds selectors to businessEndpoints

--- src/businessEndpoints.ts
+++ src/businessEndpoints.ts
@@ -7,6 +7,7 @@ export const businessEndpoints: Endpoints = {
     },
     timeLastRan: "updateTermsTimeLastRan",
     runStatus: "updateTermsRunStatus",
+    selector: "updateTermsSelect",
   },
   occupancy_buildLedgerTimeLastRan: {
     action: () => {
@@ -14,6 +15,7 @@ export const businessEndpoints: Endpoints = {
     },
     timeLastRan: "buildLedgerTimeLastRan",
     runStatus: "buildLedgerRunStatus",
+    selector: "buildLedgerSelect",
   },
   // TODO: selector endpoint functionality may be removed in the future, as the speed floor doesn't let them feel good, and their functionality may not be needed.
   // occupancy_updateTermsSelect: {
