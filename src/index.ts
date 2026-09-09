38c12d9b34711d1b1bc399b86018ed833bdb0d11
Byron Broughten <byron.broughten@gmail.com>
2026-09-08 23:05:42 -0500

    Restores generateConfigFiles

--- src/index.ts
+++ src/index.ts
@@ -35,6 +35,11 @@ function _indexMainTest() {
   mainTests[nameOfTestToRun]();
 }
 
+function generateConfigFiles() {
+  // needed for npm run gen:configs
+  return ConfigOrchestrator.init().generateConfigFiles();
+}
+
 function triggerOnEdit(e: GoogleAppsScript.Events.SheetsOnEdit) {
   if (Api.isSuspectedApiCall(e)) {
     Api.init(businessEndpoints).handleSheetOnEditEvent(e);
